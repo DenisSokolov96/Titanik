28a889c26364ee68d4c5e1813fcca2d4ccbe09a5
DenisSokolov96 <dens111296@gmail.com>
2019-12-06 12:54:45 +0300

    финал

--- main.py
+++ main.py
@@ -16,21 +16,21 @@ def Titanic():
     # удаляем из входов зависимую перменную и незначимые  признаки
     X.drop(['Survived', 'Name', 'PassengerId', 'Ticket'], axis=1, inplace=True)
     X.head()
-    X.info()
+    #X.info()
     # в поле Cabin много пропусков, удалим и его
     X.drop(['Cabin'], axis=1, inplace=True)
     X['Embarked'].describe()
     # дозаполняем пропуски
     X['Age'].fillna(X['Age'].median(), inplace=True)
     X['Embarked'].fillna('S', inplace=True)
-    X.info()
+    #X.info()
     # кодируем поле Embarked методом дамми-кодирования
     X = pd.concat([X, pd.get_dummies(X['Embarked'], prefix="Embarked")], axis=1)
     # удаляем старое поле Embarked
     X.drop(['Embarked'], axis=1, inplace=True)
     # кодируем поле обычным способом (0 и 1)
     X['Sex'] = pd.factorize(X['Sex'])[0]
-    X.info()
+    #X.info()
     # делим выборку на обучающую и тестовую
     X_train = X[:-200]
     X_test = X[-200:]
@@ -42,17 +42,16 @@ def Titanic():
     clf.score(X_train, y_train)
     clf.score(X_test, y_test)
 
-    rfc = RandomForestClassifier(n_estimators=10, max_depth=5, random_state=21)
-    rfc.fit(X_train, y_train)
-    rfc.score(X_test, y_test)
+    #rfc = RandomForestClassifier(n_estimators=10, max_depth=5, random_state=21)
+    #rfc.fit(X_train, y_train)
+    #rfc.score(X_test, y_test)
 
     eli5.explain_weights_sklearn(clf, feature_names=X_train.columns.values)
-    plot_tree(clf, filled=True)  # построить дерево с заполнением
+
+    plot_tree(clf, filled=True)
     plt.show()
 
     export_graphviz(clf, out_file='datas/pic.dot')
-
-    #export_graphviz(clf, out_file='datas/pic.dot', feature_names=X_train.columns, filled=True)
 
 
 if __name__ == '__main__':
