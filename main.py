d678160978163fb9ca0f16f2cbbb9e7fa7969893
Alexander Biryulyov <medvedkama@icloud.com>
2019-12-06 15:18:04 +0300

    add graph

--- main.py
+++ main.py
@@ -14,6 +14,17 @@ def Titanic():
     td.isnull().sum()
     sns.heatmap(td.isnull(), cbar=False).set_title("Missing values heatmap")
     plt.show()
+    data = {'apples': 10, 'oranges': 15, 'lemons': 5, 'limes': 20}
+    names = list(data.keys())
+    values = list(data.values())
+
+    fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+    axs[0].bar(names, values)
+    axs[1].scatter(names, values)
+    axs[2].plot(names, values)
+    fig.suptitle('Categorical Plotting')
+    td.Survived()
+    plt.show()
 
 
     print(td.nunique())
